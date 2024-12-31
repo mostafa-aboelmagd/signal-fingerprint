3281a189e9d3e87cb4c5e6bcbab004fab33cbf42
Mosafa Ayman <mostafaaboelmagd01@gmail.com>
2024-12-31 03:31:26 +0200

    Initial Logic Done

---
+++ ComputeHashedFeatures.py
@@ -0,0 +1,48 @@
+import librosa
+import numpy as np
+import imagehash
+from PIL import Image
+
+# Extract features from the WAV file
+def extractFeatures(wavFile):
+    loadedSound, samplingRate = librosa.load(wavFile, sr=None) # loadedSound is an array containing the amplitudes, sr is set to none so that it uses the original sampling rate
+
+    melSpectrogram = librosa.feature.melspectrogram(y=loadedSound, sr=samplingRate, n_mels=128, fmax=8000) # Computes mel spectrogram, which is designed to mimic human hearing
+    # Most audio content does not typically need frequencies higher than 8 kHz, therefore we chose it to limit unneccesary computations
+    
+    features = {
+        "mfcc": librosa.feature.mfcc(y=loadedSound, sr=samplingRate), # Mel-Frequency Cepstral Coefficients represents power spectrum (timbre or quality) of the sound
+        "chroma": librosa.feature.chroma_stft(y=loadedSound, sr=samplingRate), # Represents harmonic content (energy distribution across 12 different pitch classes)
+        "spectralContrast": librosa.feature.spectral_contrast(y=loadedSound, sr=samplingRate), # Represents difference in amplitude between peaks and valleys
+        "spectralRollof": librosa.feature.spectral_rolloff(y=loadedSound, sr=samplingRate), # Measures the frequency below which a certain percentage (85%) of the total spectral energy lies
+        "melSpectrogram": librosa.power_to_db(melSpectrogram, ref=np.max) # Converts the mel spectrogram to log scale, compressing loud values and making the quieter parts more visible
+    }
+
+    return features
+
+# Normalize features and convert to a 2D array
+def normalizeFeatures(features):
+
+    featuresList = []
+    # Normalize each feature between 0 and 255 as perceptual hashing will treat the features as an image object
+    for key in features:
+        currFeature = features[key]
+        featuresList.append((currFeature - np.min(currFeature)) / (np.max(currFeature) - np.min(currFeature)) * 255)
+
+    # Combine features into a 2D array (stack rows vertically)
+    featuresMatrix = np.vstack(featuresList).astype(np.uint8)
+    return featuresMatrix
+
+# Compute the perceptual hash
+def hashFeatures(featuresMatrix):
+    image = Image.fromarray(featuresMatrix) # Converts an array into an image object
+    hashedFeatures = imagehash.phash(image) # Creates a hash value that represents the visual (perceptual) content of an image ignoring slight variations that aren't percieved
+    return str(hashedFeatures) # Hexadecimal representation of the hashing making it easier to store
+
+# Process the hashed features
+def processHash(wavFile):
+    features = extractFeatures(wavFile)
+    featuresMatrix = normalizeFeatures(features)
+    featuresHash = hashFeatures(featuresMatrix)
+
+    return featuresHash
--- GenerateFeaturesDatabase.py
+++ GenerateFeaturesDatabase.py
@@ -1,52 +1,7 @@
-import librosa
-import numpy as np
-import imagehash
-from PIL import Image
 import shutil
 import json
 from pathlib import Path
-
-# Extract features from the WAV file
-def extractFeatures(wavFile):
-    loadedSound, samplingRate = librosa.load(wavFile, sr=None) # loadedSound is an array containing the amplitudes, sr is set to none so that it uses the original sampling rate
-    features = {
-        "mfcc": librosa.feature.mfcc(y=loadedSound, sr=samplingRate).mean(axis=1), # Mel-Frequency Cepstral Coefficients represents power spectrum (timbre) of the sound
-        "chroma": librosa.feature.chroma_stft(y=loadedSound, sr=samplingRate).mean(axis=1), # Represents harmonic content (energy distribution across 12 different pitch classes)
-        "spectralContrast": librosa.feature.spectral_contrast(y=loadedSound, sr=samplingRate).mean(axis=1), # Represents difference in amplitude between peaks and valleys
-        # Measures the frequency below which a certain percentage (85%) of the total spectral energy lies, helpful for differentiating vocals from instrumental parts
-        "spectralRollof": librosa.feature.spectral_rolloff(y=loadedSound, sr=samplingRate).mean(axis=1),
-        "onsetStrength": librosa.onset.onset_strength(y=loadedSound, sr=samplingRate), # Measures the intensity (like beats) at each frame in the audio
-    }
-    # .mean(axis=1) gets us the average value across all the columns (time), resulting in a 1D array
-    return features
-
-# Normalize features and convert to a 2D array
-def normalizeFeatures(features):
-    # Concatenate the arrays containing the features into a 1D array
-    featuresList = np.hstack([
-        features["mfcc"],
-        features["chroma"],
-        features["spectralContrast"],
-        features["spectralRollof"],
-    ])
-    # Scales the feature values so that the minimum becomes 0, and the maximum becomes 255, needed for perceptual hashing
-    featuresList = (featuresList - np.min(featuresList)) / (np.max(featuresList) - np.min(featuresList)) * 255
-    featuresList = featuresList.astype(np.uint8)
-    return featuresList
-
-# Compute the perceptual hash
-def hashFeatures(featuresList):
-    image = Image.fromarray(featuresList) # Converts an array into an image object
-    hashedFeatures = imagehash.phash(image) # Creates a hash value that represents the visual (perceptual) content of an image ignoring slight variations that aren't percieved
-    return str(hashedFeatures) # Hexadecimal representation of the hashing making it easier to store
-
-# Process the hashed features
-def processHash(wavFile):
-    features = extractFeatures(wavFile)
-    featuresList = normalizeFeatures(features)
-    featuresHash = hashFeatures(featuresList)
-
-    return featuresHash
+import ComputeHashedFeatures
 
 # Save the hashed features locally
 def saveHash(wavFile, featuresHash, outputDirectory):
@@ -71,5 +26,5 @@ if outputDirectory.exists():
         shutil.rmtree(outputDirectory)
 
 for file in inputDirectory.glob("*.wav"):
-    hashedFeatures = processHash(file)
+    hashedFeatures = ComputeHashedFeatures.processHash(file)
     saveHash(file, hashedFeatures, outputDirectory)
